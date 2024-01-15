e5184c814241be7eb721a4cd2933c59ba07b4583
lpatel14 <lpatel6789@gmail.com>
2024-01-15 11:06:17 -0800

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def main():
     # File Upload
     uploaded_file = st.file_uploader("Choose an Excel file", type=["xls", "xlsx"])
 
-    if uploaded_file is not None:
+    #if uploaded_file is not None:
 
 if __name__ == "__main__":
     main()
