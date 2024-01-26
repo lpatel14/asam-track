c4a3cb15973bbde2a2ee18a04b80be6f71ddc009
lpatel14 <lpatel6789@gmail.com>
2024-01-26 07:14:14 -0800

    Update app.py

--- app.py
+++ app.py
@@ -130,8 +130,9 @@ def main():
     data = data_main['Close'].T
     data_adjusted = data_main['Adj Close'].T
     ##Loading all stock data gonna take 3 mins to run 
-    """
+    
     data = data.T[data.columns>=transactions[transactions.Action=='Buy'].Date.max()].T
+    """
     data_adjusted = data_adjusted.T[data_adjusted.columns>=transactions[transactions.Action=='Buy'].Date.max()].T
 
     history = postions_calc[['Group','Tickers']].copy()
