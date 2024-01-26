1699e8bc3c16c31d884c75dcdd4eba4b9ff78787
lpatel14 <lpatel6789@gmail.com>
2024-01-26 07:52:00 -0800

    Update app.py

--- app.py
+++ app.py
@@ -128,6 +128,7 @@ def main():
     
     data_main = pdr.get_data_yahoo(tickers_list, start=analysis_start_date, end=analysis_end_date_plusone).dropna(axis=0,how='all') #switch this date for different cohorts
     data = data_main['Close']
+    data['Date'] = pd.to_datetime(data['Date'])
     data['Date'] = data['Date'].dt.date
     data_adjusted = data_main['Adj Close'].T
     ##Loading all stock data gonna take 3 mins to run 
