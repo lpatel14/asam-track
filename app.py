6e38d0f3c1ec35dd2918d54e8232f8e63728c82e
lpatel14 <lpatel6789@gmail.com>
2024-02-02 21:29:30 -0800

    Update app.py

--- app.py
+++ app.py
@@ -190,8 +190,7 @@ def main():
     for x in range(0, len(cols)):
         cols[x].subheader(display_port_stats.columns[x])
         for y in range(0, display_port_stats.shape[0]):
-            st.write(y)
-            cols[x].metric(display_port_stats.index[y], display_port_stats.iloc[y,x])
+            cols[x].metric(display_port_stats.index[y], display_port_stats.iloc[y,x].round(2))
         
     
 if __name__ == "__main__":
