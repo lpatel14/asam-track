dd7898650f4379c27f7cb745e108d77e192d8752
lpatel14 <lpatel6789@gmail.com>
2024-02-28 12:32:45 -0800

    Update app.py

--- app.py
+++ app.py
@@ -193,10 +193,12 @@ def main():
     sp = "^GSPC"
     sp_data = yf.download(sp, start=analysis_start_date, end=analysis_end_date_plusone)['Adj Close']
     sp_returns = sp_data.pct_change()
+    
     #sp_returns_rf = sp_returns.merge(risk_free_rate, how='left', on='Date')
-    #xcess_mkt = sp_returns['Adj Close'] - risk_free_rate['Close']
+    xcess_mkt = sp_returns['Adj Close'] - risk_free_rate['Close']
     st.write(sp_returns)
     st.write(risk_free_rate)
+    st.write(xcess_mkt)
 
     #Calculate SMB (Russell 2000 - Russell 1000)
     smb_tickers = "^RUT ^RUI"
