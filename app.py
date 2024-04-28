9b781daa7c35f821daf1753b6433a48a30a081fc
lpatel14 <lpatel6789@gmail.com>
2024-04-27 20:23:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def main():
     pos_cols = postions_calc[['Group','Tickers','Shares']].copy()
     daily_returns = pos_cols.merge(daily_returns_T,how='left',left_on=['Tickers'],right_index=True)
     daily_returns.drop_duplicates()
-
+    st.dataframe(daily_returns)
     tot_transactions = transactions.drop(['Date'], axis=1).groupby('Group').sum()['Total']
 
     merged_df = pd.merge(left=tot_transactions, right=display_Total_Value, how='left', on=['Group'])
