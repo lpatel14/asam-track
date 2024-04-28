ae63cb287653da4caa815232d0b16ae672abe50c
lpatel14 <lpatel6789@gmail.com>
2024-04-27 21:59:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,6 @@ def main():
     # Apply pct_change() along the rows (now representing dates)
     daily_returns = Total_Value.drop(['Group','Shares'], axis=1)
     daily_returns = daily_returns.set_index('Tickers', inplace=False)
-    st.dataframe(daily_returns)
     daily_returns = daily_returns.T.pct_change()
     
     daily_returns_T = daily_returns.drop(daily_returns.index[0]).T
