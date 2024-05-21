67105637d73c38b09903ddbd7eeb8f14e8568c25
lpatel14 <lpatel6789@gmail.com>
2024-05-20 23:49:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -130,7 +130,8 @@ def main():
 
     result_df = pd.DataFrame(columns=merged_df.columns)
     merged_df.reset_index(drop=False, inplace=True)
-    st.dataframe(daily_returns.T)
+    facts = daily_returns.T
+    st.dataframe(facts)
     for index, row in merged_df.iterrows():
         new_row = row.copy()
         new_row[1] = row[1]
